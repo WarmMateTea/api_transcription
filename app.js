99ad6e2f19f903a011a1c21b89951fb88093710f
WarmMateTea <mateus.hirata22@gmail.com>
2024-09-11 14:47:37 -0300

    add rota de teste de upload de arquivos, e pensamento da rota de fragmentos...

--- app.js
+++ app.js
@@ -75,6 +75,26 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
         });
 })
 
+// caminho testepra receber o arquivo de áudio + chave
+app.post('/api/test_upload', upload.single('file'), (req, res) => {
+    console.log("New Request at ", Date.now());
+
+    console.log("Uploaded file: ", req.file);
+    
+    // TODO: checar tamanho do arquivo; se maior que 25MB, fragmentar (pesquisar como :) )
+
+    const uploadedFile = req.file;
+    const filePath = path.join(__dirname, uploadedFile.path);
+    console.log("File path to transcribe: ", filePath);
+
+    if (!uploadedFile) {
+        return res.status(400).json({message: 'No file uploaded'});
+    }
+
+    console.log("WARN: This file (", uploadedFile.path ,") will need to be deleted manually.")
+    return res.status(200).json({message: 'Congrats, file received!'});
+})
+
 app.post('/api/testrequest', (req, res) => {
 
     const rawText = req.body['content'];
@@ -88,3 +108,30 @@ app.post('/api/testrequest', (req, res) => {
             return res.status(500).json({message: 'Error during external request'});
         });
 })
+
+//rotas p/ lidar com arquivos fragmentados
+app.post('/api/upload/fragment', upload.single('file'), (req, res) => {
+    // fluxo: 
+    // receber dado
+    // ver se tem a pasta certa
+    // se n tiver criar
+    // salvar na pasta
+    // enviar resposta
+
+    //no úlimo fragmento, aí sim chamar o método de transcrição
+    console.log("Fragment uploaded at", Date.now());
+})
+
+
+var fragmentHeadParser = (req, res, next) => {
+    const processId = req.headers['process_id'];
+    const segmentIndex = req.headers['segment_index'];
+    const totalSegments = req.headers['total_segments'];
+    const isLastSegment = req.headers['is_last_segment'];
+
+    if (processId == undefined || segmentIndex == undefined || totalSegments == undefined || isLastSegment == undefined) {
+        return res.status(400).json({ message: "Custom header malformed. The header should have: process_id, segment_index, total_segments, is_last_segment"});
+    }
+
+    next();
+};
